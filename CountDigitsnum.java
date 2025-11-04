f8efe31226c770ef9c7597557782e49c49d82196
shravanreddyt <160338312+shravanreddyt@users.noreply.github.com>
2025-11-04 13:12:39 +0530

    Add files via upload

---
+++ CountDigitsnum.java
@@ -0,0 +1,18 @@
+public class CountDigitsnum {
+    public static void main(String[] args){
+        
+    
+        int num = 12345;  // you can change the number
+        int count = 0;
+
+        while (num != 0) {
+            num = num / 10;  // remove last digit
+            count++;         // increase count
+        }
+
+        System.out.println("Number of digits: " + count);
+    }
+}
+    
+
+
